8150efff9889c7aae660464164047ad7bc396d06
Kwok1am <51690055+Kwok1am@users.noreply.github.com>
2021-05-25 22:10:50 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ wget -q https://www.rarlab.com/rar/rarlinux-x64-5.9.0.tar.gz
 tar xf rarlinux-x64-5.9.0.tar.gz
 export PATH=$PWD/rar:$PATH
 
-unrar -p'$SA_SECRET' e accounts.rar /app/accounts/
+unrar -p'${SA_SECRET}' e accounts.rar /app/accounts/
 
 # Install aria2c static binary
 wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
