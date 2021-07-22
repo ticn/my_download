924eb49c115b1700e52265e7bc2d958ea80f664c
Kwok1am <51690055+Kwok1am@users.noreply.github.com>
2021-07-22 23:08:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -224,11 +224,11 @@ async function updateDB(){
 
         var original_magnet = $('div.blockcode > div > ol > li').text();
         if (original_magnet.includes('magnet')){
-	    var magnet = original_magnet(/(magnet:\?xt=urn:btih:)[0-9a-zA-Z]{40}/);
-	    if(!magnet){
-		var magnet = original_magnet(/(magnet:\?xt=urn:btih:)[0-9a-zA-Z]{32}/);
-	    }
-	    if(magnet){
+	       var magnet = original_magnet(/(magnet:\?xt=urn:btih:)[0-9a-zA-Z]{40}/);
+	       if(!magnet){
+                var magnet = original_magnet(/(magnet:\?xt=urn:btih:)[0-9a-zA-Z]{32}/);
+	       }
+	       if(magnet){
                 await client.query(update_query_text,[magnet,url]);
             }
         }
