3e4df9e58cc927c1632ba457803afa3319aa1d5f
Kwok1am <51690055+Kwok1am@users.noreply.github.com>
2021-07-23 00:58:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -222,11 +222,10 @@ async function updateDB(){
         const html = respone.body;
         const $ = cheerio.load(html);
 
-        var magnet = $('div.blockcode > div > ol > li').text();
-        if (magnet.includes('magnet')){
-	       var magnet = magnet.match(/magnet:\?xt=urn:btih(?:(?!magnet).)*/);
-           await client.query(update_query_text,[magnet,url]);
-        }
+        var original_magnet = $('div.blockcode > div > ol > li').text();
+	var magnet = original_magnet.match(/magnet:\?xt=urn:btih(?:(?!magnet).)*/);
+            await client.query(update_query_text,[magnet,url]);
+	    
     }
     await Promise.map(postsWithoutMagnet,parseMagnet,{concurrency:32});
 }
