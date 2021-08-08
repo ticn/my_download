b8d8f6a4761240e59ec0b32f8d7473f0baf5dc14
Kwok1am <51690055+Kwok1am@users.noreply.github.com>
2021-08-08 15:13:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ async function updateDB(){
 
 async function cleanOldPost(latestDate){
     const query = `select * FROM posts
-    where postdate <  TO_DATE($1,'YYYY-MM-DD') - INTERVAL '2 days';`;
+    where postdate <  TO_DATE($1,'YYYY-MM-DD') - INTERVAL '30 days';`;
     const result = await client.query(query,[latestDate]);
     for (const doc of result.rows){
         const title = doc['title'];
